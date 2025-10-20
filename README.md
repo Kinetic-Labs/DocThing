e0e309d45cc9d1dfc14d753121970251593c4584
Thoq <tristanshaw000@gmail.com>
2025-10-20 15:08:53 -0400

    idk

--- README.md
+++ README.md
@@ -1,3 +1,64 @@
 # DocThing
 
 Generate documentation from HTML or code comments
+
+::: Roadmap
+  ## Roadmap
+
+  Todo: implement comment parsing
+
+## Usage
+Simply specify the input
+`docthing <input.md>`
+
+and optionally specify the output file:
+`docthing <input.md> <output.md>`
+
+## Here are some examples
+
+::: Example Markdown
+  ### Headings
+  Headings are the standard `#` symbol
+  ```md
+  # H1
+  ## H2
+  ### H3
+  ```
+  this goes on up to h6
+
+  ### Typing
+  Typing is just like Markdown, using asteriks to denote a certain effect
+  ```md
+  *Italic*
+  **Bold**
+  ***Italic and Bold***
+  ```
+
+  ### Codeblocks
+  Uses backticks like Markdown
+  `inline code`
+
+  usual three backticks (`Shift + ~`)
+
+  ### Blockquote
+  Uses the chevron
+  ```md
+  > Blockquote
+  ```
+
+  ### Dropdowns
+  Now dropdowns are handled differently than Markdown.
+  Markdown does **not** handle it very well,
+  DocThing uses three colons (`Shift + ;`, `:`) and the format is:
+  ```
+  ::: title
+    (indented)
+  ```
+
+  so it can be written as:
+  ```md
+  ::: im a dropdown
+    ### Hello!
+  ```
+
+  More examples will be comming soon
--- src/features/Md.hs
+++ src/features/Md.hs
@@ -1,19 +1,26 @@
-{-# LANGUAGE OverloadedStrings #-}
-
---------------------------------------------------
+----------------------------------
 -- Md.hs
 -- Markdown related functions
 --
 -- @since 0.1.0.0
 -- @author Thoq
---------------------------------------------------
+----------------------------------
+{-# LANGUAGE OverloadedStrings #-}
 
 module Md (convertMdToHtml) where
 
 import Data.Text (Text)
 import qualified Data.Text as T
 
---------------------------------------------------
+----------------------------------
+
+escapeHtml :: Text -> Text
+escapeHtml =
+  T.replace (T.pack "<") (T.pack "&lt;")
+    . T.replace (T.pack ">") (T.pack "&gt;")
+    . T.replace (T.pack "'") (T.pack "&#39;")
+
+----------------------------------
 
 convertMdToHtml :: Text -> Text
 convertMdToHtml = T.unlines . convertLines False . T.lines
@@ -21,55 +28,57 @@ convertMdToHtml = T.unlines . convertLines False . T.lines
     convertLines :: Bool -> [Text] -> [Text]
     convertLines _ [] = []
     convertLines inCodeBlock (line : rest)
-      | "```" `T.isPrefixOf` line =
+      | (T.pack "```") `T.isPrefixOf` line =
           if inCodeBlock
-            then "</code></pre>" : convertLines False rest
-            else "<pre><code>" : convertLines True rest
-      | inCodeBlock = line : convertLines True rest
-      | "::: " `T.isPrefixOf` line =
+            then (T.pack "</code></pre>") : convertLines False rest
+            else (T.pack "<pre><code>") : convertLines True rest
+      | inCodeBlock = escapeHtml line : convertLines True rest
+      | (T.pack "::: ") `T.isPrefixOf` line =
           let title = T.drop 4 line
               (dropdownLines, remainingLines) = span isDropdownContent rest
               indentedContent = map unindent dropdownLines
               dropdownHtml = convertMdToHtml (T.unlines indentedContent)
-           in ("<details><summary>" <> title <> "</summary><div class=\"details-content\">")
+           in ((T.pack "<details><summary>") <> title <> (T.pack "</summary><div class=\"details-content\">"))
                 : T.lines dropdownHtml
-                ++ ["</div></details>"]
+                ++ [T.pack "</div></details>"]
                 ++ convertLines False remainingLines
-      | "###### " `T.isPrefixOf` line = ("<h6>" <> T.drop 7 line <> "</h6>") : convertLines False rest
-      | "##### " `T.isPrefixOf` line = ("<h5>" <> T.drop 6 line <> "</h5>") : convertLines False rest
-      | "#### " `T.isPrefixOf` line = ("<h4>" <> T.drop 5 line <> "</h4>") : convertLines False rest
-      | "### " `T.isPrefixOf` line = ("<h3>" <> T.drop 4 line <> "</h3>") : convertLines False rest
-      | "## " `T.isPrefixOf` line = ("<h2>" <> T.drop 3 line <> "</h2>") : convertLines False rest
-      | "# " `T.isPrefixOf` line = ("<h1>" <> T.drop 2 line <> "</h1>") : convertLines False rest
-      | otherwise = ("<p>" <> parseInline line <> "</p>") : convertLines False rest
+      | (T.pack "> ") `T.isPrefixOf` line =
+          let (quoteLines, remainingLines) = span ((T.pack "> ") `T.isPrefixOf`) (line : rest)
+              unquotedLines = map (T.drop 2) quoteLines
+              quoteHtml = convertMdToHtml (T.unlines unquotedLines)
+           in (T.pack "<blockquote>") : T.lines quoteHtml ++ [T.pack "</blockquote>"] ++ convertLines False remainingLines
+      | (T.pack "###### ") `T.isPrefixOf` line = ((T.pack "<h6>") <> T.drop 7 line <> (T.pack "</h6>")) : convertLines False rest
+      | (T.pack "##### ") `T.isPrefixOf` line = ((T.pack "<h5>") <> T.drop 6 line <> (T.pack "</h5>")) : convertLines False rest
+      | (T.pack "#### ") `T.isPrefixOf` line = ((T.pack "<h4>") <> T.drop 5 line <> (T.pack "</h4>")) : convertLines False rest
+      | (T.pack "### ") `T.isPrefixOf` line = ((T.pack "<h3>") <> T.drop 4 line <> (T.pack "</h3>")) : convertLines False rest
+      | (T.pack "## ") `T.isPrefixOf` line = ((T.pack "<h2>") <> T.drop 3 line <> (T.pack "</h2>")) : convertLines False rest
+      | (T.pack "# ") `T.isPrefixOf` line = ((T.pack "<h1>") <> T.drop 2 line <> (T.pack "</h1>")) : convertLines False rest
+      | otherwise = ((T.pack "<p>") <> parseInline line <> (T.pack "</p>")) : convertLines False rest
       where
-        isDropdownContent s = T.null s || "  " `T.isPrefixOf` s
+        isDropdownContent s = T.null s || (T.pack "  ") `T.isPrefixOf` s
         unindent s
-          | "  " `T.isPrefixOf` s = T.drop 2 s
+          | (T.pack "  ") `T.isPrefixOf` s = T.drop 2 s
           | otherwise = s
 
---------------------------------------------------
+----------------------------------
 
 parseInline :: Text -> Text
 parseInline text
-  | T.null text = ""
-  | "**" `T.isPrefixOf` text =
-      let (boldText, rest) = spanUntil "**" (T.drop 2 text)
-       in "<strong>" <> parseInline boldText <> "</strong>" <> parseInline (T.drop 2 rest)
-  | "*" `T.isPrefixOf` text =
-      let (italicText, rest) = spanUntil "*" (T.drop 1 text)
-       in "<em>" <> parseInline italicText <> "</em>" <> parseInline (T.drop 1 rest)
-  | "`" `T.isPrefixOf` text =
-      let (inlineCode, rest) = spanUntil "`" (T.drop 1 text)
-       in "<code>" <> parseInline inlineCode <> "</code>" <> parseInline (T.drop 1 rest)
-  | ">" `T.isPrefixOf` text =
-      let (blockquote, rest) = spanUntil "\n" (T.drop 1 text)
-       in "<blockquote>" <> parseInline blockquote <> "</blockquote>" <> parseInline (T.drop 1 rest)
+  | T.null text = T.pack ""
+  | (T.pack "**") `T.isPrefixOf` text =
+      let (boldText, rest) = spanUntil (T.pack "**") (T.drop 2 text)
+       in (T.pack "<strong>") <> parseInline boldText <> (T.pack "</strong>") <> parseInline (T.drop 2 rest)
+  | (T.pack "*") `T.isPrefixOf` text =
+      let (italicText, rest) = spanUntil (T.pack "*") (T.drop 1 text)
+       in (T.pack "<em>") <> parseInline italicText <> (T.pack "</em>") <> parseInline (T.drop 1 rest)
+  | (T.pack "`") `T.isPrefixOf` text =
+      let (inlineCode, rest) = spanUntil (T.pack "`") (T.drop 1 text)
+       in (T.pack "<code>") <> escapeHtml inlineCode <> (T.pack "</code>") <> parseInline (T.drop 1 rest)
   | otherwise = T.cons (T.head text) (parseInline (T.tail text))
 
---------------------------------------------------
+----------------------------------
 
 spanUntil :: Text -> Text -> (Text, Text)
 spanUntil delim str = T.breakOn delim str
 
---------------------------------------------------
+----------------------------------
